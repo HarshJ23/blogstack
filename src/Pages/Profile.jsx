71ca342d01805ad22994274d7bb048803a6e7184
HarshJ23 <hjawajiwar@gmail.com>
2023-06-02 20:57:04 +0530

    Profile data detched from firebase

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
 import { getAuth } from 'firebase/auth'
 import { useNavigate } from 'react-router';
 import {VscSignOut} from 'react-icons/vsc'
@@ -9,7 +9,18 @@ export default function Profile() {
 
 const auth=getAuth();
 const navigate = useNavigate();
+const user = auth.currentUser; 
 
+const [profileData , setProfileData] = useState(
+  {
+    name: auth.currentUser.displayName,
+    email : auth.currentUser.email,
+  }
+);
+
+const {name , email} = profileData;
+
+// signout functionality
 function onLogout()
 {
   auth.signOut();
@@ -18,6 +29,27 @@ function onLogout()
 
 }
 
+
+// to fetch the profile details of  current user
+useEffect(()=>{
+  
+  function fetchUser()
+  {
+    if (user !== null) {
+      // The user object has basic properties such as display name, email, etc.
+      const displayName = user.displayName;
+      const email = user.email;
+      const uid = user.uid;
+
+      console.log(displayName,email,uid);
+    }
+
+  }
+  fetchUser();
+
+},[user])
+
+
   return (
     <div className='w-full items-center flex flex-col'>
     {/* <button className="btn btn-primary " onClick={onLogout}>SIGN OUT <VscSignOut className='h-6 w-6 m-2'/></button> */}
@@ -25,8 +57,8 @@ function onLogout()
 <h1 className="normal-case text-3xl mt-3 font-bold">My Profile</h1>
 
       <div className=' flex flex-col '>
-      <input type="text" placeholder="Name" className="input input-bordered border-orange-500 input-warning w-full max-w-xs mt-6" />
-      <input type="text" placeholder="Email address " className="input input-bordered border-orange-500 input-warning w-full max-w-xs mt-6" />
+      <input type="text" placeholder="Name" value={name} className="input input-bordered border-orange-500 input-warning w-full max-w-xs mt-6" />
+      <input type="text" placeholder="Email address" value={email} className="input input-bordered border-orange-500 input-warning w-full max-w-xs mt-6" />
       
       <div className='flex flex-row mt-6 space-x-6'>
 
