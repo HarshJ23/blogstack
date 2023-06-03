ed3919384ed6588a27e1ce17e7cd0c387e5f5183
HarshJ23 <hjawajiwar@gmail.com>
2023-06-03 15:27:44 +0530

    text-area

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
-import Navbar from './Components/Navbar';
 import SignIn from './Pages/SignIn';
-import PrivateRoute from './Components/PrivateRoute';
+import PrivateRoute from './Components/PrivateRoute'
 import PublishBlog from './Pages/PublishBlog';
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+// const location = useLocation();
+
   return (
   <>
   
   <Router>
     <Navbar/>
+  
     <Routes>
-      <Route path="/"  element={<Home/>}/>
+    <Route path="/"  element={<Home/>}/>
 
-      <Route path="/profile" element={<PrivateRoute/>}>
-      <Route path="/profile"  element={<Profile/>}/>
-      </Route>
+    <Route path="/profile" element={<PrivateRoute/>}>
+    <Route path="/profile"  element={<Profile/>}/>
+    </Route>
 
-      <Route path="/publish-blogs" element={<PrivateRoute/>}>
-      <Route path="/publish-blogs"  element={<PublishBlog/>}/>
-      </Route>
+    <Route path="/publish-blogs" element={<PrivateRoute/>}>
+    <Route path="/publish-blogs"  element={<PublishBlog/>}/>
+    </Route>
 
-      <Route path="/sign-in"  element={<SignIn/>}/>
-    </Routes>
+    <Route path="/sign-in"  element={<SignIn/>}/>
+  </Routes>
+   
   </Router>
   
   <ToastContainer
--- src/Pages/PublishBlog.jsx
+++ src/Pages/PublishBlog.jsx
@@ -1,24 +1,68 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+
 export default function PublishBlog() {
 
     // const navigate = useNavigate();
 
+    const [text, setText] = useState('');
+    const [rows, setRows] = useState(1);
+
+
+    const handleTitleChange = (event) => {
+      const { value } = event.target;
+      setText(value);
+  
+      // Calculate the number of rows based on the textarea's scroll height
+      // const textareaLineHeight = 16; // Adjust this value to match your textarea's line-height
+      const rowsToShow = Math.ceil( 0.8*value.length / event.target.cols) || 1;
+      setRows(rowsToShow/2);
+    };
+
+    const handleKeyDown = (event) => {
+      const { value } = event.target;
+      if (event.key === 'Backspace') {
+        // Calculate the number of rows based on the textarea's scroll height
+        // const textareaLineHeight = 16; // Adjust this value to match your textarea's line-height
+        const rowsToShow = Math.ceil( 0.8*value.length / event.target.cols) || 1;
+        setRows(rowsToShow);
+      }
+    };
+  
+
+    const handleContentChange=(event)=>{
+      const {Contentvalue} = event.target; 
+      setText(Contentvalue);
+    }
+
   return (
     <main >
-        <h1 className='text-3xl normal-case font-bold mt-3 ml-2'>Publish your blog  <span  className='text-orange-500'>(1/3)</span></h1>
+        <h1 className='text-3xl normal-case font-bold mt-3 ml-2'>Publish your blog</h1>
 
 
-        <div className='flex flex-col mt-6 ml-2 mx-auto'>
+        <div className='flex flex-col mt-6  mx-auto   w-2/3 border-4  '>
             
-        <div class="form-control w-full max-w-xs">
-  <label class="label">
-    <span class="label-text text-2xl font-bold">Title </span>
-  </label>
-  <input type="text" placeholder="Enter the title of your article" class="input input-bordered w-full max-w-xs" />
+        <div class="form-control  flex flex-col   w-full  border-4">
+
+ <textarea value={text}
+      rows={rows}
+      onChange={handleTitleChange}
+      onKeyDown={handleKeyDown} className="textarea textarea-ghost focus:outline-0 focus:border-r-0 focus:border-y-0  focus:bg-base-200  focus:border-l-orange-500 focus:border-l-2 text-2xl font-semibold overflow-hidden" placeholder="Title">
+</textarea>
+
+<textarea 
+      rows={rows}
+      onChange={handleContentChange}
+      onKeyDown={handleKeyDown} className="textarea mt-14 textarea-ghost focus:outline-0 focus:border-r-0 focus:border-y-0  focus:bg-base-200 focus:border-l-2 text-base font-semibold overflow-hidden" placeholder="Article">
+</textarea>
+
+
 </div>
 
         </div>
+
+
     </main>
   )
 }
